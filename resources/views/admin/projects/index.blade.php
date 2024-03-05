33ff020c50b2e0f568af6eb8fb394751b2fe3a22
MirkoCifelli <ciffo4@hotmail.it>
2024-03-05 17:11:17 +0100

    Add index.blade

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('page-title', 'Tutti i Project')
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center text-success">
+                        Tutti i Project
+                    </h1>
+
+                    <div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Slug</th>
+                                    <th scope="col">Created at</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($projects as $projet)
+                                    <tr>
+                                        <th scope="row">{{ $projet->id }}</th>
+                                        <td>{{ $projet->title }}</td>
+                                        <td>{{ $projet->slug }}</td>
+                                        <td>{{ $projet->created_at }}</td>
+                                        <td>
+                                            {{-- <a href="{{ route('admin.projects.show', ['projet' => $projet->slug]) }}" class="btn btn-xs btn-primary">
+                                                Show
+                                            </a> --}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
